848fc8b017652592fa49fc4626ff399c544cb5ee
Mirko Rossini <mirko.rossini@ymail.com>
2015-01-24 14:36:44 -0500

    Restyling forms

--- src/main/python/altauth/forms.py
+++ src/main/python/altauth/forms.py
@@ -8,54 +8,54 @@ class SetAlternativePasswordForm(forms.Form):
     """
     Form to store an alternative password for a user
     """
-    passphrase = forms.CharField(max_length=128, required = False)
-    salt = forms.CharField(max_length=128, required = False)
+    passphrase = forms.CharField(max_length=128, required=False)
+    salt = forms.CharField(max_length=128, required=False)
 
 
 class SetPublicKeyForm(forms.Form):
     """
     Form to store a public key for the user
     """
-    public_key = forms.CharField(widget=forms.Textarea, max_length=500,required = True)
-    pubkey_type = forms.ChoiceField(choices = ALLOWED_PUBLICKEY_TYPES, required = False, initial = 'RSA')
-
+    public_key = forms.CharField(widget=forms.Textarea, max_length=500, required=True)
+    pubkey_type = forms.ChoiceField(choices=ALLOWED_PUBLICKEY_TYPES, required=False, initial='RSA')
 
 
 class AltauthLoginForm(forms.Form):
     username = forms.CharField(max_length=254)
     error_messages = AuthenticationForm.error_messages
+
     def __init__(self, *args, **kwargs):
         """
-        This form works exactly like the AuthenticationForm in 
+        This form works exactly like the AuthenticationForm in
         django.contrib.auth
         """
         self.user_cache = None
         super(AltauthLoginForm, self).__init__(*args, **kwargs)
 
-    def custom_validation(self, message = 'Could not validate user'):
+    def custom_validation(self, message='Could not validate user'):
         if self.user_cache is None:
             raise forms.ValidationError(
-                   message
-                )
+                message
+            )
         elif not self.user_cache.is_active:
             raise forms.ValidationError(self.error_messages['inactive'])
 
+
 class PublicKeyLoginForm(AltauthLoginForm):
     """
-    Form to log in using a public key encrypted token 
+    Form to log in using a public key encrypted token
     """
     token = forms.CharField(max_length=254)
 
-
     def clean(self):
-        
         username = self.cleaned_data.get('username')
         token = self.cleaned_data.get('token')
         if username and token:
-            self.user_cache = authenticate(username = username,
-                                           token = token)
+            self.user_cache = authenticate(username=username,
+                                           token=token)
             self.custom_validation("dio porco")
         return self.cleaned_data
+
 
 class AlternativePasswordLoginForm(AltauthLoginForm):
     """

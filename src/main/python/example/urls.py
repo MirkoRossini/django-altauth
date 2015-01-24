b579b6d7b67f4bf5deda47fa6ce3a907feb623fd
Mirko Rossini <mirko.rossini@ymail.com>
2015-01-24 14:55:39 -0500

    Restyling urls

--- src/main/python/example/urls.py
+++ src/main/python/example/urls.py
@@ -4,11 +4,12 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'example.views.home', name='home'),
     url(r'^altauth/', include('altauth.urls')),
-    (r'^accounts/login/$', 'django.contrib.auth.views.login'),    
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
 
 
     # Uncomment the admin/doc line below to enable admin documentation:
